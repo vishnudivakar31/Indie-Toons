bcd6d5989ef5d86060c3bd40a05f956750e9c504
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-06-25 18:22:17 -0400

    initial commit for usermanagement microservice

---
+++ indie-toons-usernamangement/src/main/java/io/wanderingthinkter/indietoonsusernamangement/IndieToonsUsernamangementApplication.java
@@ -0,0 +1,13 @@
+package io.wanderingthinkter.indietoonsusernamangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndieToonsUsernamangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndieToonsUsernamangementApplication.class, args);
+	}
+
+}
---
+++ indie-toons-usernamangement/src/test/java/io/wanderingthinkter/indietoonsusernamangement/IndieToonsUsernamangementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wanderingthinkter.indietoonsusernamangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndieToonsUsernamangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
