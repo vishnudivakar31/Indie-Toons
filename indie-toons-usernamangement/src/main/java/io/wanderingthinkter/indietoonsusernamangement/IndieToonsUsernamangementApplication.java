a0ee5c3830f9ae495b539f9fef2fc6d625fc607b
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-06-26 13:38:33 -0400

    smtp debug false

--- indie-toons-usernamangement/src/main/java/io/wanderingthinkter/indietoonsusernamangement/IndieToonsUsernamangementApplication.java
+++ indie-toons-usernamangement/src/main/java/io/wanderingthinkter/indietoonsusernamangement/IndieToonsUsernamangementApplication.java
@@ -41,7 +41,6 @@ public class IndieToonsUsernamangementApplication {
 		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.debug", "true");
 
 		return mailSender;
 	}
