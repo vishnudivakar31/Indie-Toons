2fbc723a24322475b85c8e85c9a6f021e5068394
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-06-30 10:37:05 -0400

    media directory

--- mediaservice/src/main/java/io/wanderingthinkter/mediaservice/MediaserviceApplication.java
+++ mediaservice/src/main/java/io/wanderingthinkter/mediaservice/MediaserviceApplication.java
@@ -1,13 +1,18 @@
 package io.wanderingthinkter.mediaservice;
 
+import io.wanderingthinkter.mediaservice.utils.Constants;
+import io.wanderingthinkter.mediaservice.utils.FileUploadUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class MediaserviceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(MediaserviceApplication.class, args);
+		FileUploadUtil.createDirectory(Constants.MEDIA_DIRECTORY);
 	}
 
 }
---
+++ mediaservice/src/main/java/io/wanderingthinkter/mediaservice/utils/Constants.java
@@ -0,0 +1,5 @@
+package io.wanderingthinkter.mediaservice.utils;
+
+public class Constants {
+    public static final String MEDIA_DIRECTORY = "media";
+}
