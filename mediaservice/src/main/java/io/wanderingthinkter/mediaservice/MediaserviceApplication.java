1861ee7a4b040cb21a87cb1c9018d7be7c83fee9
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-06-30 09:37:13 -0400

    media service first commit

---
+++ mediaservice/src/main/java/io/wanderingthinkter/mediaservice/MediaserviceApplication.java
@@ -0,0 +1,13 @@
+package io.wanderingthinkter.mediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaserviceApplication.class, args);
+	}
+
+}
---
+++ mediaservice/src/test/java/io/wanderingthinkter/mediaservice/MediaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wanderingthinkter.mediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
