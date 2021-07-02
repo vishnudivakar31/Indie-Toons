124ca30cdff9327c8cbe2a28a1fbc4ed6b4d1431
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-07-02 10:23:13 -0400

    indie-service initial commit

---
+++ indie-service/src/main/java/io/wanderingthinkter/indieservice/IndieServiceApplication.java
@@ -0,0 +1,13 @@
+package io.wanderingthinkter.indieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndieServiceApplication.class, args);
+	}
+
+}
---
+++ indie-service/src/test/java/io/wanderingthinkter/indieservice/IndieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wanderingthinkter.indieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
