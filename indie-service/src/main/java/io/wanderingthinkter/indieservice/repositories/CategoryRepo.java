f35250dda46575dbd600d6962378a49434207045
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-07-03 12:13:04 -0400

    repository tag bug fix

--- indie-service/src/main/java/io/wanderingthinkter/indieservice/repositories/CategoryRepo.java
+++ indie-service/src/main/java/io/wanderingthinkter/indieservice/repositories/CategoryRepo.java
@@ -2,6 +2,8 @@ package io.wanderingthinkter.indieservice.repositories;
 
 import io.wanderingthinkter.indieservice.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepo extends JpaRepository<Category, Long> {
 }
